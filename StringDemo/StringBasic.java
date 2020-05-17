b497523a6ea91bebac573fee11f0f26fd486b84d
GaneshGudekar <65483502+GaneshGudekar@users.noreply.github.com>
2020-05-17 12:58:56 +0530

    Add files via upload

---
+++ StringDemo/StringBasic.java
@@ -0,0 +1,86 @@
+package StringDemo;
+
+import java.util.StringTokenizer;
+
+public class StringBasic
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		
+		// String as datatype
+		
+		/*
+		String name = "Ganesh";
+		
+		name +=" I am java developer";
+		name +=" I am free lancer";
+		
+		// name = name + " "
+		
+		System.out.println("Name =" +name);
+		*/
+		
+		/*
+		StringBuilder sb = new StringBuilder();
+		sb.append("I am java Developer ");
+		sb.append(1000);
+		
+		System.out.println(sb.toString());
+		*/
+		
+		/*
+		StringBuffer sb = new StringBuffer();
+		sb.append(203737.8888);
+		sb.append("I am java Developer ");
+		sb.append(1000);
+		
+		
+		System.out.println(sb);
+		
+		*/
+		
+		
+		/*
+		StringTokenizer st = new StringTokenizer("My name is  Ganesh , I am java Developer");
+		
+		
+		while(st.hasMoreTokens())
+		{
+			System.out.println(st.nextToken(","));
+		}
+		*/
+		
+		
+		String name = "JAVA";
+		
+		int l =name.length();
+		System.out.println("Length of string = "+ l);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
