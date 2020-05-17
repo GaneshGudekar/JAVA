bad24483c406764aa5ef5746d358fa01b3a9fbd6
GaneshGudekar <65483502+GaneshGudekar@users.noreply.github.com>
2020-05-17 12:47:43 +0530

    Add files via upload

---
+++ DEMO.java
@@ -0,0 +1,25 @@
+
+public class DEMO {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		/*
+		 * System.out.println("Hello world");
+		 * System.out.println("Welcome in Java Program");
+		 * 
+		 * System.out.println("Welcome in Online Tutorials"); System.out.println();
+		 */
+	
+		
+		int no = 10;
+		
+		System.out.println("The value of NO = " + no);
+		
+		
+		
+		
+	}
+
+}
---
+++ ScanInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ScanInput 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc = new Scanner(System.in);
+		
+		/*
+		 * System.out.println("Enter the no"); int no = sc.nextInt();
+		 * 
+		 * System.out.println("The value of no = " + no);
+		 */
+		
+		System.out.println("Enter the name");
+		//String name = sc.next();
+		String name = sc.nextLine();
+		
+		System.out.println("Name = "+name);
+		
+		
+		
+		
+		
+	}
+
+}
